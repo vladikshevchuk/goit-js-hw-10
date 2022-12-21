d83badf23eafa5b2355b60a8a5543a67912da09f
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2022-12-21 12:30:07 +0100

    error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
+import debounce from "lodash.debounce";
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+// fetchCountries();
+
+const inputEl = document.querySelector('input');
+inputEl.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(evt) {
+    const textContent = evt.target.value;
+
+    if (textContent === '') {
+        return
+    }
+
+    const country = textContent.trim();
+
+    fetchCountries(country);
+}
